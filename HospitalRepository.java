54f00d81b924481421128e9069044e0c23900650
SoundaryaVeer <103375830+SoundaryaVeer@users.noreply.github.com>
2022-04-15 18:27:42 +0530

    Hospital

---
+++ HospitalRepository.java
@@ -0,0 +1,25 @@
+package com.bloodbank.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.data.jpa.repository.Query;
+
+
+import com.bloodBank.model.Hospital;
+
+public interface HospitalRepository extends JpaRepository<Hospital, Long> {
+	
+	@Query(value = "SELECT * FROM hospital WHERE email = :email and password= :password", nativeQuery = true)
+	Hospital bloodBankLogin(String email,String password);
+	
+	@Query(value = "SELECT * FROM hospital WHERE location = :location", nativeQuery = true)
+	List<Hospital> getHospitalByLocation(String location);
+	
+	@Query(value = "DELETE  FROM hospital WHERE email = :email", nativeQuery = true)
+	int deleteByEmail(String email);
+	
+	@Query(value = "SELECT *  FROM hospital WHERE email = :email", nativeQuery = true)
+	Hospital findOneByEmail(String email);
+}
