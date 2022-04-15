4c7bbab424cd1d7074a1a607b147917a00bdb75a
SoundaryaVeer <103375830+SoundaryaVeer@users.noreply.github.com>
2022-04-15 18:32:09 +0530

    Changed spelling mistake

--- HospitalController.java
+++ HospitalController.java
@@ -15,8 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bloodBank.dao.HospitalDAO;
-import com.bloodBank.model.Hospital;
+import com.bloodbank.dao.HospitalDAO;
+import com.bloodbank.model.Hospital;
 
 @RestController
 @RequestMapping("/")
